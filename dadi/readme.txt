76f3e7157086e0cc0d05df8450431d53ec2eab0f
Fededada23 <dadamofederico@gmail.com>
2022-11-30 15:47:12 +0100

    fixed if

---
+++ dadi/readme.txt
@@ -0,0 +1,8 @@
+GIOCO DEI DADI
+
+1-Generare un numero casuale per il giocatore
+2-Generare un numero casuale per il computer
+3-Calcolare il valore piu alto
+    3.1 SE il giocatore ha il valore piu alto stampa hai vinto
+    3.2 SE il computer ha il valore piu alto stampa il computer ha vinto
+    3.3 ALTRIMENTI se i valori sono uguali stampa non ci sono ne vincitori ne vinti
--- mail/js/script.js
+++ mail/js/script.js
@@ -2,14 +2,14 @@ const email = ['ciao@gmail.com','filippo@gmail.com', 'ciccio@mastrobattista.com'
 let cerca = prompt('inserisci mail per accedere');
 let flag = false;
 
-for(let i=0; i<email; i++){
+for(let i=0; i<email.length; i++){
 
     if(email[i] == cerca){
         flag = true;
     }
 }
 
-if(flag = true){
+if(flag){
     console.log('email presente nella lista')
 }
 
